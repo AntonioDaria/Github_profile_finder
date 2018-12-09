97c3ed4fb2d7602f2ece2f4b9f82d25e679e90b3
Antonio <antoniodaria@msn.com>
2018-12-09 10:28:26 +0000

    Can see user name, user avatar and link to profile

--- js/main.js
+++ js/main.js
@@ -10,16 +10,20 @@ $(document).ready(function(){
           client_secret:'a2b698bf7e7c02f898197cf136d1a41f704ca8e4'
         }
     }).done(function(user){
-      $.ajax({
-        url:'https://api.github.com/users/'+username+'/repos',
-        data:{
-          client_id:'b9315bcd5a07fcd759d8',
-          client_secret:'a2b698bf7e7c02f898197cf136d1a41f704ca8e4',
-          sort: 'created: asc'
-        }
-      }).done(function(repos){
-        console.log(repos);
-      });
-    });
-  });
-});
+      $('#profile').html(`
+       <div class="card border-primary mb-3" style="max-width: 100rem;">
+         <div class="card-header"><h3>${user.name}</h3></div>
+         <div class="card-body">
+           <div class="row">
+           <div class="col-md-3">
+             <img class="img-thumbnail avatar" src="${user.avatar_url}">
+             <a target="_blank" class="btn btn-primary btn-block" href="${user.html_url}">View Profile</a>
+           </div>
+         </div>
+       </div>
+       <h3 class="page-header">Latest Repos</h3>
+       <div id="repos"></div>
+       `);
+     });
+   });
+ });
