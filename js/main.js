1e11972cd212ef9cdd68b214f1a4cded2ea6d7cb
Antonio <antoniodaria@msn.com>
2018-12-09 10:04:39 +0000

    Ajax call to api returns repos objects

--- js/main.js
+++ js/main.js
@@ -10,8 +10,16 @@ $(document).ready(function(){
           client_secret:'a2b698bf7e7c02f898197cf136d1a41f704ca8e4'
         }
     }).done(function(user){
-      console.log(user);
-    
+      $.ajax({
+        url:'https://api.github.com/users/'+username+'/repos',
+        data:{
+          client_id:'b9315bcd5a07fcd759d8',
+          client_secret:'a2b698bf7e7c02f898197cf136d1a41f704ca8e4',
+          sort: 'created: asc'
+        }
+      }).done(function(repos){
+        console.log(repos);
+      });
     });
   });
 });
