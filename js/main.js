e2bacaba65495a692e3773ed665d4f66c7696ff3
Antonio <antoniodaria@msn.com>
2018-12-09 09:55:18 +0000

    Ajax call to api returns user object

---
+++ js/main.js
@@ -0,0 +1,17 @@
+
+$(document).ready(function(){
+  $('#searchUser').on('keyup', function(e){
+    let username = e.target.value;
+
+    $.ajax({
+        url:'https://api.github.com/users/'+username,
+        data:{
+          client_id:'b9315bcd5a07fcd759d8',
+          client_secret:'a2b698bf7e7c02f898197cf136d1a41f704ca8e4'
+        }
+    }).done(function(user){
+      console.log(user);
+    
+    });
+  });
+});
